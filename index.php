8111b0fe2e8a5e08a11e87ff9095cfeb883de68b
Preston-D <38825956+Preston-D@users.noreply.github.com>
2025-11-08 09:55:39 -0800

    blur + parallax

--- index.php
+++ index.php
@@ -5,9 +5,17 @@ include 'templates/header.php';
 <main>
     <div class="parallax image-1">
         <div class="wrapper">
-            <div class="parallax-container" data-speed="0.5">
-                <h1>Helpful Computer Service in the Seattle Area</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis sequi, cum reiciendis debitis praesentium nesciunt exercitationem impedit tenetur consequuntur, amet aliquid et earum reprehenderit totam quod nam. Exercitationem, iste maxime?</p>
+            <div class="parallax-container" data-speed="0.2">
+                <div class="bg-container-medium">
+                    <div class="blurred-background">
+                    </div>
+                    <div class="bg-container-forground">
+                        <h1><b>Helpful Computer Service<br>in the Seattle Area</b></h1>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis sequi, cum reiciendis debitis praesentium nesciunt exercitationem impedit tenetur consequuntur, amet aliquid et earum reprehenderit totam quod nam. Exercitationem, iste maxime?</p>
+                    </div>
+                </div>
+
+
             </div>
         </div>
     </div>
@@ -25,9 +33,22 @@ include 'templates/header.php';
     </div>
 
 
-    <div class="scroll-section" data-speed="1.2">
+    <div class="scroll-section" data-speed="1">
         This div is just here to enable scrolling.
     </div>
 
 </main>
 <?php include 'templates/footer.php'; ?>
+
+<script>
+    const parallaxElements = document.querySelectorAll("[data-speed]");
+
+    window.addEventListener("scroll", () => {
+        const scrollY = window.scrollY;
+
+        parallaxElements.forEach((el) => {
+            const speed = parseFloat(el.dataset.speed);
+            el.style.transform = `translateY(${scrollY * speed * -0.6}px)`;
+        });
+    });
+</script>
