2cf567f153bbda83e9b0ff626f4752486a1210f2
Preston-D <38825956+Preston-D@users.noreply.github.com>
2025-11-08 11:04:25 -0800

    parallax at a good point + nav fix

--- index.php
+++ index.php
@@ -45,6 +45,27 @@ include 'templates/header.php';
         This div is just here to enable scrolling.
     </div>
 
+    <div class="parallax image-1">
+        <div class="wrapper">
+            <div class="parallax-container" data-speed="0.2">
+                <div class="bg-container-medium">
+                    <div class="blurred-background">
+                    </div>
+                    <div class="bg-container-forground">
+                        <h1><b>Helpful Computer Service<br>in the Seattle Area</b></h1>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis sequi, cum reiciendis debitis praesentium nesciunt exercitationem impedit tenetur consequuntur, amet aliquid et earum reprehenderit totam quod nam. Exercitationem, iste maxime?</p>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+    <div class="scroll-section" data-speed="1">
+        This div is just here to enable scrolling.
+    </div>
+
 </main>
 <?php include 'templates/footer.php'; ?>
 
